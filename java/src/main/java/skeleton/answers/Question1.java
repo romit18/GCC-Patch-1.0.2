469ec9d3c1bf180e3d9743224dd52dc2a27854f5
romit18 <46364956+romit18@users.noreply.github.com>
2019-10-14 21:34:36 +0530

    Update Question1.java

--- java/src/main/java/skeleton/answers/Question1.java
+++ java/src/main/java/skeleton/answers/Question1.java
@@ -3,8 +3,21 @@ package skeleton.answers;
 public class Question1 {
 
     public static double calculateTotalPayment(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage) {
-        // TODO Auto-generated method stub
-        return -5;
+          public static double calculateTotalPayment(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage) {
+        double fixedRe=(repaymentPercentage*initialLevelOfDebt)/100;
+        if(fixedRe<50){
+          fixedRe=50;
+        }
+        double temp=initialLevelOfDebt;
+        int count=0;
+        while(temp>=50){
+            temp=temp+(temp)*(interestPercentage/100);
+            temp=temp-fixedRe;
+            count++;
+        }
+        double final1=count*fixedRe+temp+fixedRe;
+        return Math.round(final1);
+}
     }
 
 }
